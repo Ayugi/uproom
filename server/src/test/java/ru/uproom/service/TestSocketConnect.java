9fcb3d5ad7a3f10bd0190145c519e1004dcbe800
hedin <hedinant@gmail.com>
2014-08-30 17:16:50 +0400

    Test fix

--- server/src/test/java/ru/uproom/service/TestSocketConnect.java
+++ server/src/test/java/ru/uproom/service/TestSocketConnect.java
@@ -1,6 +1,7 @@
 package ru.uproom.service;
 
 import junit.framework.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 import ru.uproom.gate.transport.HandshakeCommand;
 
@@ -16,17 +17,15 @@ public class TestSocketConnect {
     public static final String TEST_GATE_NAME = "testGate";
 
     @Test
-    public void testSocketHandshake(){
+    public synchronized void testSocketHandshake() throws IOException, InterruptedException {
         GateServiceImpl service = new GateServiceImpl();
         service.init();
-        try {
-            Socket socket = new Socket("localhost", 8282);
-            ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
-            objectOutputStream.writeObject(new HandshakeCommand(TEST_GATE_NAME));
-            GateSocketHandler handler = service.getHandler(TEST_GATE_NAME);
-            Assert.assertNotNull(handler);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+
+        Socket socket = new Socket("localhost", 8282);
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+        objectOutputStream.writeObject(new HandshakeCommand(TEST_GATE_NAME));
+        wait(10);
+        GateSocketHandler handler = service.getHandler(TEST_GATE_NAME);
+        Assert.assertNotNull(handler);
     }
 }
