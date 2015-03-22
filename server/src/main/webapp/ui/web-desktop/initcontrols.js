c4950629d4c7d8902f4c367bdfbc8460c11cabf9
ayugi-dev <ydev@freshcube.ru>
2015-03-22 20:48:59 +0300

    UI: colorpicker in devicelist

--- server/src/main/webapp/ui/web-desktop/initcontrols.js
+++ server/src/main/webapp/ui/web-desktop/initcontrols.js
@@ -2,7 +2,7 @@
  * Created by Ayugi on 08.01.2015.
  */
     // Prepare X-edit
-$(document).ready(function() {
+$(document).ready(function () {
     //toggle `popup` / `inline` mode
 //        $.fn.editable.defaults.mode = 'inline';
 
@@ -14,4 +14,12 @@ $(document).ready(function() {
 
     $('#dp5').slider();
     $('#dp6').slider();
+
+    var cw1 =
+        Raphael.colorwheel($("#cw1"), 250, 180).color("#00F");
+
+    cw1.onchange(function (color) {
+        var colors = [parseInt(color.r), parseInt(color.g), parseInt(color.b)]
+        $("#color-mon").css("background", color.hex).text("RGB: " + colors.join(", "))
+    })
 });
